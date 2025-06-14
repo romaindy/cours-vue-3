a8942a46bceedec1d1f660ef2257d476fff6d1ac
Romain <romain.divandary@gmail.com>
2025-06-14 12:01:12 +0200

    feat: add components to checkout form

---
+++ src/components/FormAddress.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const address = ref({
+  address: '',
+  post_code: '',
+  city: '',
+})
+</script>
+<template>
+  <div>
+    <label for="address">Adresse</label>
+    <input id="address" type="text" required v-model="address.address" />
+  </div>
+  <div>
+    <label for="post_code">Code Postal</label>
+    <input id="post_code" type="text" required v-model="address.post_code" />
+  </div>
+  <div>
+    <label for="city">Ville</label>
+    <input id="city" type="text" required v-model="address.city" />
+  </div>
+</template>
