b70223bbb5fa98dcdaa04c2b540e2f744fa3e21d
Romain <romain.divandary@gmail.com>
2026-02-25 17:10:17 +0100

    feat: move name and lastName in billing

--- src/components/Checkout/FormBilling.vue
+++ src/components/Checkout/FormBilling.vue
@@ -2,6 +2,20 @@
 import FormAddress from '@/components/FormAddress.vue'
 </script>
 <template>
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <label for="name">Nom</label>
+      <input id="name" type="text" required />
+    </div>
+    <div>
+      <label for="firstname">Prénom</label>
+      <input id="firstname" type="text" required />
+    </div>
+  </div>
+  <div>
+    <label for="email">Email</label>
+    <input id="email" type="email" required />
+  </div>
   <h2>Adresse de facturation</h2>
   <FormAddress />
   <div>
--- src/components/Checkout/FormCheckout.vue
+++ src/components/Checkout/FormCheckout.vue
@@ -4,9 +4,6 @@ import FormBilling from '@/components/Checkout/FormBilling.vue'
 import FormShipping from '@/components/Checkout/FormShipping.vue'
 
 const checkout = ref({
-  name: '',
-  firstname: '',
-  email: '',
   shipToDifferentAddress: false,
 })
 const onSubmit = () => {}
@@ -14,20 +11,6 @@ const onSubmit = () => {}
 <template>
   <form @submit="onSubmit()" class="max-w-3xl mx-auto my-8">
     <h1>Votre commande</h1>
-    <div class="grid grid-cols-2 gap-4">
-      <div>
-        <label for="name">Nom</label>
-        <input id="name" type="text" required v-model="checkout.name" />
-      </div>
-      <div>
-        <label for="firstname">Prénom</label>
-        <input id="firstname" type="text" required v-model="checkout.firstname" />
-      </div>
-    </div>
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="email" required v-model="checkout.email" />
-    </div>
     <FormBilling />
     <div>
       <input
